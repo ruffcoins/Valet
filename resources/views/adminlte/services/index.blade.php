a7dfb614692d82d5c4131b001b33946c435c987b
Anthonynzube <nzubeanthony4@gmail.com>
2020-05-09 04:20:13 +0100

    refactor: Remove view button

--- resources/views/adminlte/services/index.blade.php
+++ resources/views/adminlte/services/index.blade.php
@@ -77,11 +77,6 @@
                           {{$service->price}}
                       </td>
                       <td class="project-actions text-right">
-                          <a class="btn btn-primary btn-sm" href="#">
-                              <i class="fas fa-folder">
-                              </i>
-                              View
-                          </a>
                           <a class="btn btn-info btn-sm" href="{{route('editService', $service->id)}}">
                               <i class="fas fa-pencil-alt"></i>
                               Edit
